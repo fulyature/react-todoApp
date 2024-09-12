ebbe0ad78ef10d4e4d95709f31d9d5d8fb8d0226
fulya_ture <fulyature@gmail.com>
2024-09-12 16:50:31 +0300

    added delete btn

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-
+import { data } from "../helper/data";
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([...data]);
 
   const handleAdd = () => {
     const newTodo = {
       val: inputValue,
       id: todos.length,
+      completed: false,
     };
 
     setTodos([...todos, newTodo]);
@@ -15,19 +16,45 @@ const ToDoList = () => {
   };
   //   console.log(inputValue);
 
+  const tamamlanmisGorev = (id) => {
+    setTodos(
+      todos.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
   return (
-    <div>
+    <div className="container">
       <div className="todo-app">
         <input
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter new todo..."
         />
         <button onClick={handleAdd}>ToDo Add</button>
       </div>
-      <div>
+      <div className="todo-wrapper">
         {todos.map((item) => (
-          <div key={item.id}>
-            <p>{item.val}</p>
+          <div
+            className="gorev"
+            key={item.id}
+            onDoubleClick={() => tamamlanmisGorev(item.id)}
+          >
+            <p
+              style={{
+                textDecoration: item.completed && "line-through",
+                background: item.completed && "#83a60377",
+              }}
+            >
+              {item.val}
+            </p>
+            <button
+              onClick={() =>
+                setTodos((prev) => prev.filter((a) => a.id !== item.id))
+              }
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
